cc3c83accb6cddb835555b2794cc43addf40e063
Mishal Khalid <itsmishalk@gmail.com>
2025-09-22 22:51:22 +0500

    Create program1.c inside lab folder

---
+++ PFLabWork/program1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+	  printf("\t\t\t\t\tPlay beep five times!\n");
+    printf("\a");  // First beep
+    printf("\a");  // Second beep
+    printf("\a");  // Third beep
+    printf("\a");  // Fourth beep
+    printf("\a");  // Fifth beep
+
+    return 0;
+}
