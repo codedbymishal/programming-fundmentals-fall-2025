900b623880166fa227e92abceb8649bfb3c47899
Mishal Khalid <itsmishalk@gmail.com>
2025-10-14 21:31:33 +0500

    Lab 2 Task 4

--- Lab Work/Lab 2 Tasks/Lab 2 Task 4.c
+++ Lab Work/Lab 2 Tasks/Lab 2 Task 4.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 int main()
 {
-	int testInteger=3000000000;
-	printf("Number is %d",testInteger);
-	// In C, an int datatype usually stores values up to 2,147,483,647 
-    //but since 3,000,000,000,is larger than the max limit of int
-	//it causes integer overflow
-	//the actual output will be an incorrect negative number not what we wanted to store
+	printf("*********     ****       *         *\n");
+	printf("*       *    *    *     ***       * *\n");
+	printf("*       *   *      *   *****     *   *\n");		
+	printf("*       *   *      *     *      *     *\n");
+	printf("*       *   *      *     *      *     *\n");
+	printf("*       *   *      *     *       *   *\n");
+	printf("*       *    *    *      *        * *\n");
+	printf("*********     ****       *         *\n");
+	return 0;
+	
 }
