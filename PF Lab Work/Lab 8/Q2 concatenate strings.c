550e1f71cfc97acd38f4f58befa10bd78c5b273e
Mishal Khalid <itsmishalk@gmail.com>
2025-10-24 20:31:49 +0500

    Q2 concatenate strings

---
+++ PF Lab Work/Lab 8/Q2 concatenate strings.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main() 
+{
+ char a[20] = "Good ";
+ char b[] = "Morning";
+ 
+ strcat(a, b);//concatenate function
+ printf("Result: %s", a);
+ 
+ return 0;
+}
+
+
+    
+
