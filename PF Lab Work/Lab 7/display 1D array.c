1b51218da6d4bf61f6a2157a239aa624b9195e1c
Mishal Khalid <itsmishalk@gmail.com>
2025-10-24 20:12:10 +0500

    display array

---
+++ PF Lab Work/Lab 7/display 1D array.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() 
+{
+	 int nums[5] = {10, 20, 30, 40, 50};//array decalaration and initialisation
+     printf("\nThe third element is: %d", nums[2]);
+     
+ return 0;
+}
