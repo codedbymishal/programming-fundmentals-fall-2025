2ccfde1e1eb0fe838bf0c98f854dd52096b2f454
Mishal Khalid <itsmishalk@gmail.com>
2025-10-24 20:23:47 +0500

    Q1 name array

---
+++ PF Lab Work/Lab 8/Q1 name array.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include<string.h>
+int main() 
+{
+	printf("\n\t\t\t\t\t\tStrings in C!\n\n");
+	char name[] = "Mishal";//saves name as an array of characters
+    printf("Hello, %s!\n", name);
+    return 0;
+}
