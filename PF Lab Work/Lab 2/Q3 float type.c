4746e37b976a9562da5ff94842222587c5642451
Mishal Khalid <itsmishalk@gmail.com>
2025-10-25 09:45:59 +0500

    Q3 float type

---
+++ PF Lab Work/Lab 2/Q3 float type.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{
+    float num;
+    printf("Enter a decimal number: ");
+    scanf("%f", &num);
+    printf("You entered: %0.3f\n", num);
+    printf("Data type in C: Float!\n");
+    return 0;
+}
+
