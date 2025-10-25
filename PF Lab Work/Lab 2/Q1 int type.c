398e346bbd83878e1571c927ff5ddd219c74a1b8
Mishal Khalid <itsmishalk@gmail.com>
2025-10-25 09:45:31 +0500

    Q1 int type

---
+++ PF Lab Work/Lab 2/Q1 int type.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("You entered: %d\n", num);
+    printf("Data type in C: int!\n");
+    return 0;
+}
+
