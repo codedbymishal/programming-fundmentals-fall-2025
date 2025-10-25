cc74a4e82bb307e9c21cfd860c96341418c6bf9c
Mishal Khalid <itsmishalk@gmail.com>
2025-10-25 09:41:27 +0500

    Q3 info

---
+++ PF Lab Work/Lab 1/Q3 info.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+    int age = 18;
+    float marks = 89.5;
+    char grade = 'A';
+
+    printf("\nAge : %d\nMarks : %0.2f\nGrade : %c\n",age,marks,grade);
+    
+    return 0;
+}
