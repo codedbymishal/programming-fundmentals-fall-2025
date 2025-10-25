7a80ef473ecde30c775229968826fd6ecf8e3214
Mishal Khalid <itsmishalk@gmail.com>
2025-10-25 09:53:15 +0500

    Q1 arithmetic operations

---
+++ PF Lab Work/Lab 3/Q1 arithmetic operations.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() 
+{
+    int a = 10, b = 5;
+    
+    printf("Sum: %d + %d = %d\n", a, b, a + b);
+    printf("Difference: %d - %d = %d\n", a, b, a - b);
+    printf("Product: %d * %d = %d\n", a, b, a * b);
+    printf("Quotient: %d / %d = %d\n", a, b, a / b);
+    printf("Remainder: %d %% %d = %d\n", a, b, a % b);
+
+    return 0;
+}
+
