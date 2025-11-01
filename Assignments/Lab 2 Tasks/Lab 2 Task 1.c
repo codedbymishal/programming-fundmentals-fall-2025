bc80eaa266688c3153b1a1ccc950dfe741e9fad8
Mishal Khalid <itsmishalk@gmail.com>
2025-11-01 23:45:22 +0500

    moved Lab 2 from Lab Work to Assignments folder

---
+++ Assignments/Lab 2 Tasks/Lab 2 Task 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+	int count;
+	printf("\n\t\t\t\t\t\tPlay Beep 5 Times!!\n\n");
+	printf("\a");
+	printf("\a");
+	printf("\a");
+	printf("\a");
+	printf("\a");
+ 
+    return 0;
+}
+
---
+++ Assignments/Lab 2 Tasks/Lab 2 Task 3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+    printf("\n*\n");
+    printf("**\n");
+    printf("***\n");
+    printf("****\n");
+    printf("\n\n");
+    printf("*\n");
+    printf("*\t*\n");
+    printf("*\t*\t*\n");
+    printf("*\t*\n");
+    printf("*\n");
+    
+    return 0;
+}
+
