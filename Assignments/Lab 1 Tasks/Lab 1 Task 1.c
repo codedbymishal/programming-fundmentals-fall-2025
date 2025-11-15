0ad19aef027595007720bdbd93fa8e6ecd20af2e
Mishal Khalid <itsmishalk@gmail.com>
2025-11-15 21:12:54 +0500

    Lab 1 Task 1

---
+++ Assignments/Lab 1 Tasks/Lab 1 Task 1.c
@@ -0,0 +1,18 @@
+Write a pseudocode to find the maximum of 3 numbers a,b, and c.
+  
+Start
+    Input a, b, c
+    If a > b then
+        If a > c then
+            Print a
+        Else
+            Print c
+        End If
+    Else
+        If b >c then
+            Print b
+        Else
+            Print c
+        End If
+    End If
+End
