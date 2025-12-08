d7159e83a2f32eff4aa68280a8435df7b4f9e14d
Mishal Khalid <itsmishalk@gmail.com>
2025-12-08 22:58:47 +0500

    Swapping with pointers

---
+++ Practise/Q10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a, b, *p1, *p2, temp;
+    printf("Enter two numbers:\n ");
+    scanf("%d %d", &a, &b);
+    
+    printf("Before swapping: a = %d, b = %d\n", a, b);
+    
+    p1 = &a; p2 = &b;
+    temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    return 0;
+}
+
