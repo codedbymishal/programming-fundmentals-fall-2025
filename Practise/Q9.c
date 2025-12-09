ba52d1b49c2a8ff0e6d2285b8b36276ec4307810
Mishal Khalid <itsmishalk@gmail.com>
2025-12-09 06:41:22 +0500

    Q9.c

--- Practise/Q9.c
+++ Practise/Q9.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
 
-int main() 
-{
-    int n, t1=0, t2=1, next, i;
-    printf("Enter terms for Fibonacci Series: ");
-    scanf("%d", &n);
-    printf("Fibonacci series: ");
-    for(i=1; i<=n; i++) 
+int main() {
+    int terms, t1 = 0, t2= 1, next, i;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &terms);
+
+    if(terms <= 0) 
 	{
+        printf("Enter a positive number of terms.\n");
+        return 0;
+    }
+
+    printf("Fibonacci Series: ");
+
+    // print first two terms
+    if(terms >= 1)
         printf("%d ", t1);
-        next = t1 + t2;
+    if(terms >= 2)
+        printf("%d ", t2);
+
+    // print remaining terms
+    for(i = 3; i <= terms; i++) 
+	{
+        next = t1+t2;
+        printf("%d ", next);
         t1 = t2;
         t2 = next;
     }
+
     printf("\n");
     return 0;
 }
