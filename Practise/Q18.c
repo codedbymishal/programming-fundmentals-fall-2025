7874e5af01e722d430bd54ab023d09c492e44dd7
Mishal Khalid <itsmishalk@gmail.com>
2025-12-09 08:37:55 +0500

    Left shift

---
+++ Practise/Q18.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() 
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int d = 2,i;  // number of positions to rotate
+
+    int temp[d];  // temporarily store first d elements
+
+    // Step 1: Copy first d elements to temp
+    for ( i = 0; i < d; i++) 
+	{
+        temp[i] = arr[i];
+    }
+
+    // Step 2: Shift the remaining elements to the left
+    for ( i = d; i < n; i++) 
+	{
+        arr[i - d] = arr[i];
+    }
+
+    // Step 3: Copy the temp elements to the end
+    for ( i = 0; i < d; i++) 
+	{
+        arr[n - d + i] = temp[i];
+    }
+
+  
+    printf("Rotated array: ");
+    for ( i = 0; i < n; i++) 
+	{
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
